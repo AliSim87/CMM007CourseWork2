70ae7d476a2c0378f1411ffcf55ae92e6683b6c6
AliSim87 <0706693@rgu.ac.uk>
2019-03-26 18:59:22 +0000

    26/3/19

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Photo Contest</title>
+</head>
+<body>
+<header>
+    <nav class="navbar-nav">
+        <a class="navbar-brand" href="#">Logo</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Guideline</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Gallery</a>
+            </li>
+        </ul>
+
+    </nav>
+</header>
+
--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rober
- * Date: 26/03/2019
- * Time: 13:21
- */
-
-echo "Test";
-
+<?php include 'header.php' ?>
