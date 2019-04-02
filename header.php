b17d3d0df320cd1a1636584af65cfbd98c085372
AliSim87 <0706693@rgu.ac.uk>
2019-04-02 14:52:54 +0100

    26/3/19

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -11,6 +12,7 @@
 </head>
 <body>
 <header>
+    <div class="container">
     <nav class="navbar-nav navbar-expand-sm">
         <a class="navbar-brand" href="#">Logo</a>
         <ul class="navbar-nav">
@@ -29,5 +31,6 @@
             <li class="nav-item"><a class="nav-link" href="#"><span class="fas fa-sign-in-alt"></span> Login</a></li>
         </ul>
     </nav>
+    </div>
 </header>
 
