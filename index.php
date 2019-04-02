283adf96c9b3f0ef0e20726872702b34f954305f
AliSim87 <0706693@rgu.ac.uk>
2019-04-02 14:57:04 +0100

    2/4/19

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php include 'header.php' ?>
+
+<main>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1>Bootstrap Tutorial</h1>
+            <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+        </div>
+    </div>
+</main>
