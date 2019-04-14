247a427a13f98c15d488c85d43dd0b3eeaca873a
AliSim87 <0706693@rgu.ac.uk>
2019-04-14 16:26:25 +0100

    2/4/19

--- dbconnect.php
+++ dbconnect.php
@@ -4,8 +4,4 @@ define('DB_USERNAME', 'azure');
 define('DB_PASSWORD', '6#vWHD_$');
 define('DB_DATABASE', 'cmm007');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-if ( mysqli_connect_errno() ) {
-    // If there is an error with the connection, stop the script and display the error.
-    die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 ?>
