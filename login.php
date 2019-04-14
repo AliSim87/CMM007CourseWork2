af8c8a9736c303e877f7851ac7acb8f7691d4a52
AliSim87 <0706693@rgu.ac.uk>
2019-04-14 16:01:24 +0100

    2/4/19

--- login.php
+++ login.php
@@ -5,10 +5,10 @@
         <form action="authentication.php" method="post">
             <h2 class="text-center">Log in</h2>
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Email Address" required="required">
+                <input type="text" class="form-control" name="username" placeholder="Email Address" required="required">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" placeholder="Password" required="required">
+                <input type="password" class="form-control" name="password" placeholder="Password" required="required">
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary btn-block">Log in</button>
