1058ffe281eca354a0b1ea7afa2e7d73b92a6d1f
Alasdair <0706693@rgu.ac.uk>
2019-04-15 15:53:03 +0100

    15/4/19

--- assets/scripts/loginauthentication.php
+++ assets/scripts/loginauthentication.php
@@ -13,7 +13,7 @@
         $result=mysqli_query($db,$sql);
         if(mysqli_num_rows($result) == 1)
         {
-            header("location: userprofile.php"); // Redirecting To another Page
+            header("location: ../userprofile.php"); // Redirecting To another Page
             setcookie('loggedin',$username,time()+3600);
             $userlevel="SELECT user_level FROM users WHERE username='$username' and password='$password'";
             setcookie('accesslevel',$userlevel,time()+3600);
