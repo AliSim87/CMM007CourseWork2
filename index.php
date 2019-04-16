14fc281761fed1412014bea0c6ed2b9a66e85154
Alasdair <0706693@rgu.ac.uk>
2019-04-16 12:35:29 +0100

    15/4/19

--- index.php
+++ index.php
@@ -12,11 +12,12 @@
         </div>
     </div>
     <div class="container">
-    <div class="row">
-        <div class="col-8">
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed tempor lorem. Pellentesque sagittis ex nec velit eleifend elementum. Etiam venenatis porttitor erat in ornare. Duis nisl leo, pretium ac hendrerit pulvinar, elementum eget risus. Praesent at laoreet tellus. Nullam pharetra commodo elit ac blandit. Vivamus ac fringilla nunc.</p>
-                <p>Morbi molestie enim mattis venenatis lobortis. Cras elementum faucibus neque, non ultrices mauris molestie sit amet. Integer nec facilisis ex, nec faucibus arcu. Sed id elit sed mauris congue pretium blandit et justo. Aenean faucibus, purus id condimentum volutpat, ligula magna sagittis purus, auctor fermentum nunc quam nec orci. Maecenas accumsan felis dapibus leo efficitur, ac semper lacus dapibus. Cras non mattis est. Pellentesque rhoncus sapien tellus, in dictum justo viverra non. Aenean nisi tellus, sodales eget mauris eu, rhoncus interdum nibh. Maecenas ut feugiat purus. Cras eget purus nisl.</p></div>
+        <div class="row">
+            <div class="col-8">
+                    <h2>Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed tempor lorem. Pellentesque sagittis ex nec velit eleifend elementum. Etiam venenatis porttitor erat in ornare. Duis nisl leo, pretium ac hendrerit pulvinar, elementum eget risus. Praesent at laoreet tellus. Nullam pharetra commodo elit ac blandit. Vivamus ac fringilla nunc.</p>
+                    <p>Morbi molestie enim mattis venenatis lobortis. Cras elementum faucibus neque, non ultrices mauris molestie sit amet. Integer nec facilisis ex, nec faucibus arcu. Sed id elit sed mauris congue pretium blandit et justo. Aenean faucibus, purus id condimentum volutpat, ligula magna sagittis purus, auctor fermentum nunc quam nec orci. Maecenas accumsan felis dapibus leo efficitur, ac semper lacus dapibus. Cras non mattis est. Pellentesque rhoncus sapien tellus, in dictum justo viverra non. Aenean nisi tellus, sodales eget mauris eu, rhoncus interdum nibh. Maecenas ut feugiat purus. Cras eget purus nisl.</p>
+            </div>
             <div class="col-4 fakeimg">2 of 2</div>
         </div>
     </div>
