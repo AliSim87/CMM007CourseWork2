22d7d7f8100a45722a44991f0f04465e30ddc645
Alasdair <0706693@rgu.ac.uk>
2019-04-16 14:46:08 +0100

    15/4/19

--- loginauthentication.php
+++ loginauthentication.php
@@ -13,7 +13,8 @@
             header("location: userprofile.php"); // Redirecting To another Page
             setcookie('loggedin',$username,time()+3600);
             $sql="SELECT user_level FROM users WHERE username='$username' and password='$password'";
-            $userlevel=mysql_fetch_assoc($sql);
+            $result=mysqli_query($sql);
+            $userlevel = mysqli_fetch_object($result);
             setcookie('accesslevel',$userlevel,time()+3600);
         }else {
             echo "Incorrect username or password.";
