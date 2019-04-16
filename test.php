97ac0135d74a43867b9021d7291c2c98555eb3f0
Alasdair <0706693@rgu.ac.uk>
2019-04-16 15:04:28 +0100

    15/4/19

--- test.php
+++ test.php
@@ -3,6 +3,6 @@ include("dbconnect.php");
 $sql="SELECT user_level FROM users WHERE username='$username' and password='$password'";
 $result=$db->query($sql);
 $row=$result->fetch_array();
-$userlevel = $row->user_level;
+$userlevel = $row['user_level'];
 echo $userlevel;
 ?>
