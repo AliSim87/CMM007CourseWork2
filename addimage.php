2eeee07c17976483922b17df665bf4a90f3c62ba
Alasdair <0706693@rgu.ac.uk>
2019-04-16 10:58:36 +0100

    15/4/19

--- addimage.php
+++ addimage.php
@@ -1,15 +1,34 @@
 <?php include("dbconnect.php");
-    if (isset($_POST['upload'])) {
-        $image = $_FILES['image']['name'];
-        $image_text = mysqli_real_escape_string($db, $_POST['image_text']);
-        $target = "userimages/".basename($image);
-        $sql = "INSERT INTO images_upload (image, image_text) VALUES ('$image', '$image_text')";
-        mysqli_query($db, $sql);
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-            $msg = "Image uploaded successfully";
+$statusMsg = '';
+
+$targetDir = "userimages/";
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+    // Allow certain file formats
+    $allowTypes = array('jpg','png','jpeg','gif','pdf');
+    if(in_array($fileType, $allowTypes)){
+        // Upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            // Insert image file name into database
+            $insert = $db->query("INSERT into images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
+            if($insert){
+                $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+            }else{
+                $statusMsg = "File upload failed, please try again.";
+            }
         }else{
-            $msg = "Failed to upload image";
+            $statusMsg = "Sorry, there was an error uploading your file.";
         }
+    }else{
+        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
     }
-    $result = mysqli_query($db, "SELECT * FROM images");
+}else{
+    $statusMsg = 'Please select a file to upload.';
+}
+
+// Display status message
+echo $statusMsg;
 ?>
--- imageupload.php
+++ imageupload.php
@@ -1,29 +1,9 @@
 <?php include 'header.php' ?>
 
-<?php
-while ($row = mysqli_fetch_array($result)) {
-    echo "<div id='img_div'>";
-    echo "<img src='userimages/".$row['image']."' >";
-    echo "<p>".$row['image_text']."</p>";
-    echo "</div>";
-}
-?>
-<form method="POST" action="addimage.php" enctype="multipart/form-data">
-    <input type="hidden" name="size" value="1000000">
-    <div>
-        <input type="file" name="image">
-    </div>
-    <div>
-      <textarea
-          id="text"
-          cols="40"
-          rows="4"
-          name="image_text"
-          placeholder="Say something about this image... x"></textarea>
-    </div>
-    <div>
-        <button type="submit" name="upload">POST</button>
-    </div>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select Image File to Upload:
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
 </form>
 
 <?php include 'footer.php' ?>
