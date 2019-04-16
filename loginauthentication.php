87615a19c1054abbf949cce2a9d75adb5d1649b7
Alasdair <0706693@rgu.ac.uk>
2019-04-16 10:29:47 +0100

    15/4/19

--- loginauthentication.php
+++ loginauthentication.php
@@ -1,4 +1,4 @@
-<?php include("dbconnect.php");
+<?php include("assets\scripts\dbconnect.php");
     if(empty($_POST["username"]) || empty($_POST["password"]))
     {
         echo "Both fields are required.";
