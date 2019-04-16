2dd0545e0f76afffe862a4d62587f9e5a318a063
Alasdair <0706693@rgu.ac.uk>
2019-04-16 10:49:07 +0100

    15/4/19

--- addimage.php
+++ addimage.php
@@ -3,7 +3,7 @@
         $image = $_FILES['image']['name'];
         $image_text = mysqli_real_escape_string($db, $_POST['image_text']);
         $target = "userimages/".basename($image);
-        $sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+        $sql = "INSERT INTO images_upload (image, image_text) VALUES ('$image', '$image_text')";
         mysqli_query($db, $sql);
         if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
             $msg = "Image uploaded successfully";
