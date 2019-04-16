ec3560696c11c032f9d4e778c6be7a3d53d92fed
Alasdair <0706693@rgu.ac.uk>
2019-04-16 15:03:12 +0100

    15/4/19

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
-<?php $sql="SELECT user_level FROM users WHERE username='$username' and password='$password'";
-$result=mysqli_query($sql);
-$row=mysqli_fetch_object($result);
-$userlevel = $row->userlevel;
+<?php
+include("dbconnect.php");
+$sql="SELECT user_level FROM users WHERE username='$username' and password='$password'";
+$result=$db->query($sql);
+$row=$result->fetch_array();
+$userlevel = $row->user_level;
 echo $userlevel;
 ?>
