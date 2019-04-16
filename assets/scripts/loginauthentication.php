e38abd953164662a610ccb78fc8ae38309e16e11
Alasdair <0706693@rgu.ac.uk>
2019-04-16 10:17:32 +0100

    15/4/19

--- assets/scripts/loginauthentication.php
+++ assets/scripts/loginauthentication.php
@@ -1,5 +1,4 @@
 <?php include("dbconnect.php");
-
     if(empty($_POST["username"]) || empty($_POST["password"]))
     {
         echo "Both fields are required.";
@@ -7,14 +6,14 @@
     {
         $username=$_POST['username'];
         $password=$_POST['password'];
-
-
         $sql="SELECT id FROM users WHERE username='$username' and password='$password'";
         $result=mysqli_query($db,$sql);
         if(mysqli_num_rows($result) == 1)
         {
-            setcookie('loggedin',$username,time()+(60*60*24));
-            header("location: ..\..\userprofile.php");
+            header("location: ..\..\userprofile.php"); // Redirecting To another Page
+            setcookie('loggedin',$username,time()+3600);
+            $userlevel="SELECT user_level FROM users WHERE username='$username' and password='$password'";
+            setcookie('accesslevel',$userlevel,time()+3600);
         }else {
             echo "Incorrect username or password.";
         }
