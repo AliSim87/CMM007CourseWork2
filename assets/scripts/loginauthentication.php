30fdc70b144d216d62b61a0f476ec5b20b368ef7
Alasdair <0706693@rgu.ac.uk>
2019-04-16 09:56:35 +0100

    15/4/19

--- assets/scripts/loginauthentication.php
+++ assets/scripts/loginauthentication.php
@@ -15,8 +15,6 @@
         {
             header("location: ..\..\userprofile.php"); // Redirecting To another Page
             setcookie('loggedin',$username,time()+3600);
-            $userlevel="SELECT user_level FROM users WHERE username='$username' and password='$password'";
-            setcookie('accesslevel',$userlevel,time()+3600);
         }else {
             echo "Incorrect username or password.";
         }
