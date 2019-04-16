d9c5395ec358e382990c8ada51aba50c9efbd95f
Alasdair <0706693@rgu.ac.uk>
2019-04-16 15:31:37 +0100

    15/4/19

--- test.php
+++ test.php
@@ -4,13 +4,12 @@ include("dbconnect.php");
 $username = "AliSim";
 $password = "password1";
 
-$sql="SELECT * FROM users WHERE username='$username' and password='$password'";
+$sql="SELECT * FROM users WHERE username='AliSim'";
 $result=$db->query($sql);
-$row=$result->fetch_array();
 while($row = $result->fetch_array()){
     $userlevel = $row['user_level'];
     echo "<p>{$userlevel}</p>";
 }
 ?>
 
-<p>5</p>
+<p>6</p>
