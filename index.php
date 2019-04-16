3a3445b28564e1afdf7c4bb70b383dcf9720784c
Alasdair <0706693@rgu.ac.uk>
2019-04-16 12:32:44 +0100

    15/4/19

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 <h1>Big Blooming Aberdeen Photo Contest</h1>
                 <p><strong>Do you have a camera phone, pocket camera, dusty old camera, SLR, DSLR, mirrorless, Pinhole camera or anything in between?</strong></p>
                 <p>The point of the competition is to give you an opportunity to express yourselves about Aberdeen and your life in it. We also hope that some categories will help bring older and younger people together and some will celebrate the city’s green spaces</p>
-            </div>
+                <a class="btn btn-primary" href="signup.php" role="button">Sign Up Now</a>
         </div>
     </div>
     <div class="container">
