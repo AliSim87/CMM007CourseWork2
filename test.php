f5b56b582ac035d4fb96eed97e77df17cf21e9e6
Alasdair <0706693@rgu.ac.uk>
2019-04-17 16:04:24 +0100

    15/4/19

--- test.php
+++ test.php
@@ -8,3 +8,6 @@
 // Fix max file size
 // File structure
 // Text for site
+
+http://codewithawa.com/posts/password-reset-system-in-php
+https://www.codexworld.com/upload-store-image-file-in-database-using-php-mysql/
