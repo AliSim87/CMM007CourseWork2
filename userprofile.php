135bdd7e05d0cb3b154b9297caeebb35e44eab6b
Alasdair <0706693@rgu.ac.uk>
2019-04-17 16:03:44 +0100

    15/4/19

--- userprofile.php
+++ userprofile.php
@@ -13,8 +13,7 @@
         <div class="row">
             <div>
                 <h2>Welcome Back <?php print $_SESSION['firstname'] ?></h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed tempor lorem. Pellentesque sagittis ex nec velit eleifend elementum. Etiam venenatis porttitor erat in ornare. Duis nisl leo, pretium ac hendrerit pulvinar, elementum eget risus. Praesent at laoreet tellus. Nullam pharetra commodo elit ac blandit. Vivamus ac fringilla nunc.</p>
-                <p>Morbi molestie enim mattis venenatis lobortis. Cras elementum faucibus neque, non ultrices mauris molestie sit amet. Integer nec facilisis ex, nec faucibus arcu. Sed id elit sed mauris congue pretium blandit et justo. Aenean faucibus, purus id condimentum volutpat, ligula magna sagittis purus, auctor fermentum nunc quam nec orci. Maecenas accumsan felis dapibus leo efficitur, ac semper lacus dapibus. Cras non mattis est. Pellentesque rhoncus sapien tellus, in dictum justo viverra non. Aenean nisi tellus, sodales eget mauris eu, rhoncus interdum nibh. Maecenas ut feugiat purus. Cras eget purus nisl.</p>
+                <p><a href="imageupload.php"></a>Upload a photo</p>
             </div>
         </div>
     </div>
