51525effec60a2d3d8f322362069dc0cbc1615d3
Alasdair <0706693@rgu.ac.uk>
2019-04-17 14:34:34 +0100

    15/4/19

--- reviewphotos.php
+++ reviewphotos.php
@@ -16,23 +16,23 @@ elseif($_SESSION['user_level'] != 'admin') {
                 <?php
                 include 'dbconnect.php';
 
-                $query = $db->query("SELECT * FROM images WHERE approved=0");
+                $query = $db->query("SELECT * FROM images WHERE approved=0 AND images.user_id=users.user_id");
 
                 if($query->num_rows > 0){
                     while($row = $query->fetch_assoc()){
+
                         $imageURL = 'userimages/'.$row["file_name"];
                         $imagetitle = $row["title"];
                         $comments = $row["supporting_info"];
                         $userid = $row["user_id"];
-                        // $sql = $db->query("SELECT * FROM users WHERE user_id = '$userid'");
-                        // $result=$db->query($sql);
-                        // while($row = $result->fetch_array()){
-                            //$username = $row['username'];
-                            //?>
-                            <img src="<?php echo $imageURL; ?>" alt="<?php echo $imagetitle; ?>" class="img-thumbnail"/>
-                            <p><?php echo $imagetitle ?></p>
-                            <p>Uploaded by: <?php echo $userid ?></p>
-                            <p><?php echo $comments ?></p>
+                        $username = $row['username'];
+
+                        ?>
+
+                        <img src="<?php echo $imageURL; ?>" alt="<?php echo $imagetitle; ?>" class="img-thumbnail"/>
+                        <p><?php echo $imagetitle ?></p>
+                        <p>Uploaded by: <?php echo $username ?></p>
+                        <p><?php echo $comments ?></p>
                     <?php }
                 }else{ ?>
                     <p>No image(s) found...</p>
