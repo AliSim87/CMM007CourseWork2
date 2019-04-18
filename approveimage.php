60ea05b8d7cdb612f53d3bfa3a66e1546bdc6eae
AliSim87 <0706693@rgu.ac.uk>
2019-04-18 16:54:30 +0100

    2/4/19

--- approveimage.php
+++ approveimage.php
@@ -3,7 +3,7 @@
 $image_id = $_GET["image_id"];
 
 if(isset($_POST["submit"])) {
-    $sql="INSERT INTO images (approved) VALUES (b'1') WHERE image_id = '$image_id'";
+    $sql="UPDATE images SET approved = b'1' WHERE image_id = '$image_id'";
     if (mysqli_query($db, $sql)) {
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($db);
