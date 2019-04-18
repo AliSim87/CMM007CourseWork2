f949c994e6c919846d88ad7f9d2392ea63a59c79
AliSim87 <0706693@rgu.ac.uk>
2019-04-18 17:01:18 +0100

    2/4/19

--- approveimage.php
+++ approveimage.php
@@ -2,14 +2,11 @@
 
 $image_id = $_GET["image_id"];
 
-if(isset($_POST["submit"])) {
-    $sql="UPDATE images SET approved = 1 WHERE image_id = '$image_id'";
-    if (mysqli_query($db, $sql)) {
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($db);
-    }
-}
+$sql="UPDATE images SET approved = 1 WHERE image_id = '$image_id'";
 
-echo $image_id;
+if (mysqli_query($db, $sql)) {
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+}
 
 ?>
