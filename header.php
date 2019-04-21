8f0dd927b3d73a91bebdea0d13fb9e76a0da57b5
AliSim87 <0706693@rgu.ac.uk>
2019-04-21 23:46:36 +0100

    2/4/19

--- header.php
+++ header.php
@@ -17,8 +17,10 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="index.php"><img src="/assets/images/Bloomingbiglogo.png" alt="Big Blooming Aberdeen Logo"/></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="index.php"><img src="/assets/images/Bloomingbiglogo.png"
+                                                          alt="Big Blooming Aberdeen Logo"/></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
