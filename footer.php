c8bce8ea881e2f25a95c9f58d320519be9453995
AliSim87 <0706693@rgu.ac.uk>
2019-04-22 15:09:33 +0100

    2/4/19

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 <footer class="footer-copyright bg-light">
     <div class="container">
         <div class="text-center py-3">
-             <a href="http://acvo.org.uk/privacy-policy/">Privacy Policy</a>
-        </div>
-        <div class="text-center py-3">
+             <a href="http://acvo.org.uk/privacy-policy/">Privacy Policy</a><br />
             © 2019 Copyright: Alasdair Simpson
         </div>
     </div>
