7962930b22498004c1caedb053c00a9b2a5f816b
AliSim87 <0706693@rgu.ac.uk>
2019-04-22 10:47:12 +0100

    2/4/19

--- displaycategory.php
+++ displaycategory.php
@@ -12,44 +12,44 @@ include 'header.php'; ?>
     <main>
         <div class="container mt-5">
             <div class="row">
-                <div>
-                    <?php
-                    include 'dbconnect.php';
+                <?php
+                include 'dbconnect.php';
 
-                    $sql = $db->query("SELECT * FROM images INNER JOIN users WHERE category='$category' AND images.user_id=users.user_id");
+                $sql = $db->query("SELECT * FROM images INNER JOIN users WHERE category='$category' AND images.user_id=users.user_id");
 
-                    if ($sql->num_rows > 0) {
-                        while ($row = $sql->fetch_assoc()) {
+                if ($sql->num_rows > 0) {
+                    while ($row = $sql->fetch_assoc()) {
 
-                            $imageURL = 'userimages/' . $row['file_name'];
-                            $imageTitle = $row['title'];
-                            $firstName = $row['firstname'];
-                            $surname = $row['lastname'];
-                            $image_id = $row['image_id'];
-                            $approved = $row['approved'];
+                        $imageURL = 'userimages/' . $row['file_name'];
+                        $imageTitle = $row['title'];
+                        $firstName = $row['firstname'];
+                        $surname = $row['lastname'];
+                        $image_id = $row['image_id'];
+                        $approved = $row['approved'];
 
-                            if ($approved == 1) {
+                        if ($approved == 1) {
 
-                                ?>
-
-                                <img src="<?php echo $imageURL; ?>" alt="<?php echo $imageTitle; ?>"
-                                     class="img-thumbnail"/>
-                                <p><?php echo $imageTitle ?></p>
-                                <p>Uploaded by: <?php echo $firstName . " " . $surname ?></p>
-                                <?php if ($_SESSION['user_level'] == 'judge') { ?>
-                                    <div>
-                                        <form method="post" action="judgeimage.php?image_id=<?php echo $image_id ?>">
-                                            <button type="submit" class="btn btn-primary btn-block">Judge this photo?
-                                            </button>
-                                        </form>
-                                    </div>
-                                <?php }
-                            }
+                            ?>
+                            <div class="gallery-image">
+                            <img src="<?php echo $imageURL; ?>" alt="<?php echo $imageTitle; ?>"
+                                 class="img-thumbnail"/>
+                            <p><?php echo $imageTitle ?></p>
+                            <p>Uploaded by: <?php echo $firstName . " " . $surname ?></p>
+                            <?php if ($_SESSION['user_level'] == 'judge') { ?>
+                                <div>
+                                    <form method="post" action="judgeimage.php?image_id=<?php echo $image_id ?>">
+                                        <button type="submit" class="btn btn-primary btn-block">Judge this photo?
+                                        </button>
+                                    </form>
+                                </div>
+                                </div>
+                            <?php }
                         }
-                    } else { ?>
-                        <p>No image(s) found...</p>
-                    <?php } ?>
-                </div>
+                    }
+                } else { ?>
+                    <p>No image(s) found...</p>
+                <?php } ?>
+
             </div>
         </div>
     </main>
